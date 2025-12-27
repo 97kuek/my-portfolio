2587bde64ad9878a03519895ea927427ffca480e
97kuek <keitaro.ueki@icloud.com>
2025-12-27 10:47:26 +0900

    大規模修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,11 @@ export default defineConfig({
     site: 'https://97kuek.vercel.app',
 
     // ▼▼▼ ここを修正しました（expressiveCodeを先頭に移動） ▼▼▼
-    integrations: [expressiveCode(), mdx(), sitemap()],
+    integrations: [expressiveCode({
+        frames: {
+            showCopyToClipboardButton: true,
+        },
+    }), mdx(), sitemap()],
 
     // 2. 追加: ここで数式プラグインを有効化します
     markdown: {
